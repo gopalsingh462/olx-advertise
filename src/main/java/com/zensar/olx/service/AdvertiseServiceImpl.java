156ae3ef6fbb81d9a323bcd18d55b1afa1cb2c09
Gopal Singh <gopalsingh462@gmail.com>
2021-12-05 13:27:23 +0530

    config server added, filter method updated

--- src/main/java/com/zensar/olx/service/AdvertiseServiceImpl.java
+++ src/main/java/com/zensar/olx/service/AdvertiseServiceImpl.java
@@ -10,6 +10,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Order;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
@@ -119,28 +120,105 @@ public class AdvertiseServiceImpl implements AdvertiseService {
 		}
 	}
 
+//	@Override
+//	public List<Advertisement> searchAdsByFilter(String searchText, int categoryId, int userId, String dateCondition,
+//			LocalDate onDate, LocalDate fromDate, LocalDate toDate, String sortBy, int startIndex, int numOfRecords) {
+//		List<Map> categoriesList = masterDataDelegate.getAllCategories();
+//
+//		List<Map> statusList = masterDataDelegate.getAllStatuses();
+//		//call MasterData service getAllCategories() - RestTemplate
+//		//List<Map> categoriesList = masterDataDelegate.getAllCategories();
+//
+//		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+//		CriteriaQuery criteriaQuery = criteriaBuilder.createQuery(AdvertiseEntity.class);
+//		Root<AdvertiseEntity> rootEntity = criteriaQuery.from(AdvertiseEntity.class);
+//
+//		Predicate titlePredicate = criteriaBuilder.equal(rootEntity.get("title"), searchText); //title=searchText
+//		Predicate categoryPredicate = criteriaBuilder.equal(rootEntity.get("categoryId"), categoryId);
+//
+//		Predicate finalPredicate = criteriaBuilder.and(titlePredicate, categoryPredicate);
+//		criteriaQuery.where(finalPredicate);
+//		TypedQuery<AdvertiseEntity> query = entityManager.createQuery(criteriaQuery);
+//		List<AdvertiseEntity> advertiseEntityList = query.getResultList();
+//		List<Advertisement> adList = new ArrayList<>();
+//		for(AdvertiseEntity entity: advertiseEntityList) {
+//			adList.add(utils.getAdDtoFromAdEntity(entity));
+//		}
+//		return adList;
+//	}
+
 	@Override
-	public List<Advertisement> searchAdsByFilter(String searchText, int categoryId, int userId, String dateCondition,
-			LocalDate onDate, LocalDate fromDate, LocalDate toDate, String sortBy, int startIndex, int numOfRecords) {
-		List<Map> categoriesList = masterDataDelegate.getAllCategories();
-
-		List<Map> statusList = masterDataDelegate.getAllStatuses();
-		//call MasterData service getAllCategories() - RestTemplate
-		//List<Map> categoriesList = masterDataDelegate.getAllCategories();
-
+	public List<Advertisement> searchAdsByFilter(String searchText, int categoryId, int postedBy, String dateCondition,
+			LocalDate onDate, LocalDate fromDate, LocalDate toDate, String sortedBy, int startIndex, int records) {
 		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
-		CriteriaQuery criteriaQuery = criteriaBuilder.createQuery(AdvertiseEntity.class);
+		CriteriaQuery<AdvertiseEntity> criteriaQuery = criteriaBuilder.createQuery(AdvertiseEntity.class);
 		Root<AdvertiseEntity> rootEntity = criteriaQuery.from(AdvertiseEntity.class);
 
-		Predicate titlePredicate = criteriaBuilder.equal(rootEntity.get("title"), searchText); //title=searchText
-		Predicate categoryPredicate = criteriaBuilder.equal(rootEntity.get("categoryId"), categoryId);
+		Predicate titlePredicate = criteriaBuilder.like(rootEntity.get("title"), searchText);
+		Predicate categoryIdPredicate = criteriaBuilder.equal(rootEntity.get("categoryId"), categoryId);
+		Predicate postedByPredicate = criteriaBuilder.equal(rootEntity.get("postedBy"), postedBy);
+		Predicate onDatePredicate = criteriaBuilder.equal(rootEntity.get("createdDate"), onDate);
+		Predicate toDatePredicate = criteriaBuilder.equal(rootEntity.get("createdDate"), toDate);
+		Predicate fromDatePredicate = criteriaBuilder.equal(rootEntity.get("createdDate"), fromDate);
+		Predicate lessThanPredicate = criteriaBuilder.lessThan(rootEntity.get("createdDate"), fromDate);
+		Predicate geaterThanPredicate = criteriaBuilder.greaterThan(rootEntity.get("createdDate"), fromDate);
+		Order sortedOrder = criteriaBuilder.desc(rootEntity.get("createdDate"));
 
-		Predicate finalPredicate = criteriaBuilder.and(titlePredicate, categoryPredicate);
-		criteriaQuery.where(finalPredicate);
+		Predicate finalPredecate = null;
+		criteriaBuilder.or(titlePredicate, categoryIdPredicate, postedByPredicate);
+
+		if (searchText != null) {
+
+			if (categoryId != 0) {
+
+				if (postedBy != 0) {
+
+					if (dateCondition != null) {
+
+						switch (dateCondition) {
+
+						case "between":
+							finalPredecate = criteriaBuilder.or(titlePredicate, categoryIdPredicate, postedByPredicate,
+									fromDatePredicate, toDatePredicate);
+							break;
+						case "lessthan":
+							finalPredecate = criteriaBuilder.or(titlePredicate, categoryIdPredicate, postedByPredicate,
+									lessThanPredicate);
+							break;
+						case "greatethan":
+							finalPredecate = criteriaBuilder.or(titlePredicate, categoryIdPredicate, postedByPredicate,
+									geaterThanPredicate);
+							break;
+						case "equals":
+							finalPredecate = criteriaBuilder.or(titlePredicate, categoryIdPredicate, postedByPredicate,
+									onDatePredicate);
+							break;
+						default:
+							break;
+						}
+
+						if (sortedBy != null) {
+							criteriaQuery.orderBy(sortedOrder);
+						}
+					}
+
+				} else {
+
+				}
+
+			} else {
+
+			}
+
+		} else {
+
+		}
+
+		criteriaQuery.where(finalPredecate);
 		TypedQuery<AdvertiseEntity> query = entityManager.createQuery(criteriaQuery);
-		List<AdvertiseEntity> advertiseEntityList = query.getResultList();
+		List<AdvertiseEntity> advertiseEntities = query.getResultList();
 		List<Advertisement> adList = new ArrayList<>();
-		for(AdvertiseEntity entity: advertiseEntityList) {
+		for (AdvertiseEntity entity : advertiseEntities) {
 			adList.add(utils.getAdDtoFromAdEntity(entity));
 		}
 		return adList;
